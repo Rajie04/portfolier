fa425f4543b2be7b10d84fc506e209cef6b623c0
Raja Ahmed <rtahmed04@gmail.com>
2024-07-17 00:44:21 -0500

    add tab icon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-title-in-document-head */
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
@@ -8,6 +9,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=NTR&display=swap" rel="stylesheet" />
+        <link rel="icon" href="/glaceon.png" type="image/png" />
         <title>Raja Ahmed :D</title>
       </Head>
       <body className="bg-bodyColor w-full h-screen font-bodyFont text-textLight overflow-x-hidden overflow-y-scroll scrollbar scrollbar-track-textDark/20 scrollbar-thumb-textDark/60">
